fc3a9e7ce77e65ce3a6c9bc9ccd68309825d6250
Zhong <amtfzhongyunhe@163.com>
2017-06-29 12:42:10 +0800

    创建了一个dog类

---
+++ 美团/美团/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  美团
+//
+//  Created by 云鹤 on 2017/6/29.
+//  Copyright © 2017年 云鹤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ 美团/美团/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  美团
+//
+//  Created by 云鹤 on 2017/6/29.
+//  Copyright © 2017年 云鹤. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
